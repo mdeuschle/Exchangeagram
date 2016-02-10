b9ae96f09f1e05f910f20b87b488112e4ba221d3
Matt Deuschle <mdeuschle@gmail.com>
2016-02-10 14:27:00 -0600

    added feed

---
+++ Exchangeagram/SearchViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SearchViewController.swift
+//  Exchangeagram
+//
+//  Created by Matt Deuschle on 2/10/16.
+//  Copyright © 2016 CJM Inc. All rights reserved.
+//
+
+import UIKit
+
+
+class SearchViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+
+
+}
