a484ab19fab83df6eb8236ab8664f258cc3bf898
Matt Deuschle <mdeuschle@gmail.com>
2016-02-11 18:18:57 -0600

    added comments

--- Exchangeagram/CommentsViewController.swift
+++ Exchangeagram/CommentsViewController.swift
@@ -8,30 +8,46 @@
 
 import UIKit
 
-class CommentsViewController: UIViewController {
-    
-    @IBOutlet weak var commentTextComments: UITextField!
+import Firebase
 
-    @IBOutlet weak var sendButton: UIButton!
+class CommentsViewController: UITableViewController {
 
-    @IBOutlet weak var tableView: UITableView!
+    var posts: [String: String] = [String: String]()
+
+    @IBOutlet var commentsTableView: UITableView!
+    @IBOutlet weak var textField: UITextField!
+
+    var ref = Firebase(url: "https://exchangeogram.firebaseio.com/")
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        ref.observeEventType(.Value, withBlock:
+            {
+
+                snapshot in
+
+                self.posts = snapshot.value.objectForKey("Posts") as! [String: String]
+                print(self.posts)
+                self.tableView.reloadData()
+        })
     }
 
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 
-        return 0;
+        return self.posts.count
     }
 
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        return UITableViewCell()
+        let cell = self.tableView.dequeueReusableCellWithIdentifier("Cell")
+        var keys: Array = Array(self.posts.keys)
+        cell?.textLabel?.text = posts[keys[indexPath.row]] as String!
+
+        return cell!
     }
 
-    @IBAction func onSendButtonPressed(sender: UIButton) {
-
-    }    
-    
+    @IBAction func onSendButtonPressed(sender: UIBarButtonItem) {
+            ref.childByAppendingPath("Posts").childByAutoId().setValue(textField.text)
+    }
 }
